521e8b86bc568e71ed72842e0ff79210db5d40dd
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-08-04 13:23:20 +0200

    ok maybe not

--- lib/niko_web/live/mood_live/index.ex
+++ lib/niko_web/live/mood_live/index.ex
@@ -21,7 +21,7 @@ defmodule NikoWeb.MoodLive.Index do
       end
 
     socket = assign(socket, :current_user, current_user)
-    {:ok, stream(socket, :moods, Moods.list_moods_by_user(current_user.id))}
+    {:ok, stream(socket, :moods, Moods.list_moods())}
   end
 
   @impl true
